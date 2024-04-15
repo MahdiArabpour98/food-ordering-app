41a5b6f066ad69eb8a1b4ef6e8c55d04cb3f8bc3
MahdiArabpour <85918072+MahdiArabpour98@users.noreply.github.com>
2024-04-15 20:29:58 +0330

    create update delete

--- src/app/(admin)/menu/[id].tsx
+++ src/app/(admin)/menu/[id].tsx
@@ -1,4 +1,4 @@
-import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import { Link, Stack, useLocalSearchParams, useRouter } from "expo-router";
 import { Image, StyleSheet, Text, View, Pressable } from "react-native";
 import products from "@assets/data/products";
 import { defaultPizzaImage } from "@/components/productListItem";
@@ -6,29 +6,40 @@ import { useState } from "react";
 import Button from "@/components/Button";
 import { useCart } from "@/providers/cartProvider";
 import { PizzaSize } from "@/types";
-
-const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
+import { FontAwesome } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
 
 const ProductDetailsScreen = () => {
-  const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
   const router = useRouter();
 
   const { id } = useLocalSearchParams();
   const product = products.find((p) => p.id.toString() === id);
 
-  const { addItem } = useCart();
-
   if (!product) {
     return <Text>Product not found</Text>;
   }
 
-  const addToCart = () => {
-    addItem(product, selectedSize);
-    router.push("/cart");
-  };
-
   return (
     <View style={styles.container}>
+      <Stack.Screen
+        options={{
+          title: "Menu",
+          headerRight: () => (
+            <Link href={`/(admin)/menu/create?id=${id}`} asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="pencil"
+                    size={25}
+                    color={Colors.light.tint}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
       <Stack.Screen options={{ title: product.name }} />
       <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} />
 
